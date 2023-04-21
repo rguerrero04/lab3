fbe70591b5ee139a87d1481eb4e834a3ebb63326
Ramon Guerrero <rguerre2004@gmail.com>
2023-04-20 17:36:25 -0700

    Changes

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,12 +7,32 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+    int[] input2 = {1, 2, 3};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{3, 2, 1}, input2);
+    int[] input3 = { };
+    ArrayExamples.reverseInPlace(input3);
+    assertArrayEquals(new int[]{ }, input3);
 	}
 
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = {2, 1};
+    assertArrayEquals(new int[]{1, 2}, ArrayExamples.reversed(input1));
+    int[] input2 = {1, 2, 3};
+    assertArrayEquals(new int[]{3, 2, 1}, ArrayExamples.reversed(input2));
+    int[] input3 = { 3 };
+    assertArrayEquals(new int[]{ 3 }, ArrayExamples.reversed(input3));
+    
+  }
+
+  @Test
+  public void testaverageWithoutLowest(){
+    double[] input1 = {1};
+    double output = ArrayExamples.averageWithoutLowest(input1);
+    assertEquals(0.0, output, .0000001);
   }
 }
+
+
---
+++ ListsTests.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.*;;
+
+public class ListsTests {
+    @Test
+    public void filter(){
+        
+    }
+    
+    @Test
+    public void merge(){
+
+    }
+}
